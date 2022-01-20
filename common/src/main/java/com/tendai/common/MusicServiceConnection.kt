e6106c4419126d07cfc0debd44696694a974972c
tendai <musakanyetendai@gmail.com>
2022-01-20 19:07:57 +0200

    Add MusicServiceConnection.kt

--- common/src/main/java/com/tendai/common/MusicServiceConnection.kt
+++ common/src/main/java/com/tendai/common/MusicServiceConnection.kt
@@ -1,5 +1,117 @@
 package com.tendai.common
 
-class MusicServiceConnection {
+import android.content.ComponentName
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.session.MediaControllerCompat
+import android.support.v4.media.session.MediaSessionCompat
+import android.support.v4.media.session.PlaybackStateCompat
+import androidx.lifecycle.MutableLiveData
 
+class MusicServiceConnection(context: Context, serviceComponent: ComponentName) {
+    val isConnected = MutableLiveData<Boolean>()
+        .apply { postValue(false) }
+
+    val rootMediaId: String get() = mediaBrowser.root
+
+    val playbackState = MutableLiveData<PlaybackStateCompat>()
+        .apply { postValue(EMPTY_PLAYBACK_STATE) }
+
+    val nowPlaying = MutableLiveData<MediaMetadataCompat>()
+        .apply { postValue(NOTHING_PLAYING) }
+
+    val queueItems = MutableLiveData<List<MediaSessionCompat.QueueItem>>()
+        .apply { postValue(EMPTY_QUEUE) }
+
+    val queueTitle = MutableLiveData<CharSequence>()
+        .apply { postValue(EMPTY_QUEUE_TITLE) }
+
+    val transportControls: MediaControllerCompat.TransportControls
+        get() = mediaController.transportControls
+
+    private val mediaBrowserConnectionCallback = MediaBrowserConnectionCallback(context)
+
+    private val mediaBrowser = MediaBrowserCompat(
+        context,
+        serviceComponent,
+        mediaBrowserConnectionCallback, null
+    ).apply { connect() }
+
+    lateinit var mediaController: MediaControllerCompat
+        private set
+
+    fun subscribe(
+        parentId: String,
+        options: Bundle,
+        callback: MediaBrowserCompat.SubscriptionCallback
+    ) = mediaBrowser.subscribe(parentId, options, callback)
+
+    fun unsubscribe(parentId: String, callback: MediaBrowserCompat.SubscriptionCallback) =
+        mediaBrowser.unsubscribe(parentId, callback)
+
+    private inner class MediaControllerCallback : MediaControllerCompat.Callback() {
+        override fun onPlaybackStateChanged(state: PlaybackStateCompat?) {
+            playbackState.postValue(state ?: EMPTY_PLAYBACK_STATE)
+        }
+
+        override fun onQueueChanged(queue: MutableList<MediaSessionCompat.QueueItem>?) {
+            queueItems.postValue(queue ?: EMPTY_QUEUE)
+        }
+
+        override fun onQueueTitleChanged(title: CharSequence?) {
+            queueTitle.postValue(title ?: EMPTY_QUEUE_TITLE)
+        }
+
+        override fun onMetadataChanged(metadata: MediaMetadataCompat?) {
+            nowPlaying.postValue(metadata ?: NOTHING_PLAYING)
+        }
+
+        override fun onSessionDestroyed() {
+            mediaBrowserConnectionCallback.onConnectionSuspended()
+        }
+    }
+
+    private inner class MediaBrowserConnectionCallback(private val context: Context) :
+        MediaBrowserCompat.ConnectionCallback() {
+
+        /**
+         * Invoked after [MediaBrowserCompat.connect] when the request has successfully
+         * completed.
+         */
+        override fun onConnected() {
+            // Get a MediaController for the MediaSession.
+            mediaController = MediaControllerCompat(context, mediaBrowser.sessionToken).apply {
+                registerCallback(MediaControllerCallback())
+            }
+
+            isConnected.postValue(true)
+        }
+
+        /**
+         * Invoked when the client is disconnected from the media browser.
+         */
+        override fun onConnectionSuspended() {
+            isConnected.postValue(false)
+        }
+
+        /**
+         * Invoked when the connection to the media browser failed.
+         */
+        override fun onConnectionFailed() {
+            isConnected.postValue(false)
+        }
+    }
 }
+
+val EMPTY_PLAYBACK_STATE: PlaybackStateCompat = PlaybackStateCompat.Builder()
+    .setState(PlaybackStateCompat.STATE_NONE, 0, 0f)
+    .build()
+
+val NOTHING_PLAYING: MediaMetadataCompat = MediaMetadataCompat.Builder()
+    .putString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID, null)
+    .build()
+
+val EMPTY_QUEUE = emptyList<MediaSessionCompat.QueueItem>()
+const val EMPTY_QUEUE_TITLE = "EMPTY_QUEUE_TITLE"
