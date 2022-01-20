f76a04e65c9e75d47e78cfc7128a153c50c4d00e
tendai <musakanyetendai@gmail.com>
2022-01-20 19:09:50 +0200

    Add Kdoc

--- common/src/main/java/com/tendai/common/MusicServiceConnection.kt
+++ common/src/main/java/com/tendai/common/MusicServiceConnection.kt
@@ -9,7 +9,27 @@ import android.support.v4.media.session.MediaControllerCompat
 import android.support.v4.media.session.MediaSessionCompat
 import android.support.v4.media.session.PlaybackStateCompat
 import androidx.lifecycle.MutableLiveData
+import androidx.media.MediaBrowserServiceCompat
 
+/**
+ * Class that manages a connection to a [MediaBrowserServiceCompat] instance, typically a
+ * [MusicService] or one of its subclasses.
+ *
+ * Typically it's best to construct/inject dependencies either using DI or, as UAMP does,
+ * using [InjectorUtils] in the app module. There are a few difficulties for that here:
+ * - [MediaBrowserCompat] is a final class, so mocking it directly is difficult.
+ * - A [MediaBrowserConnectionCallback] is a parameter into the construction of
+ *   a [MediaBrowserCompat], and provides callbacks to this class.
+ * - [MediaBrowserCompat.ConnectionCallback.onConnected] is the best place to construct
+ *   a [MediaControllerCompat] that will be used to control the [MediaSessionCompat].
+ *
+ *  Because of these reasons, rather than constructing additional classes, this is treated as
+ *  a black box (which is why there's very little logic here).
+ *
+ *  This is also why the parameters to construct a [MusicServiceConnection] are simple
+ *  parameters, rather than private properties. They're only required to build the
+ *  [MediaBrowserConnectionCallback] and [MediaBrowserCompat] objects.
+ */
 class MusicServiceConnection(context: Context, serviceComponent: ComponentName) {
     val isConnected = MutableLiveData<Boolean>()
         .apply { postValue(false) }
